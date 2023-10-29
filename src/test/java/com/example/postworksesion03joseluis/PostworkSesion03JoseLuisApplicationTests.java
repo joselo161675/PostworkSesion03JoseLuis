249b74d1774a2dc2816c289abe3276b15d7dfb9d
Joselo <joselo161675@gmail.com>
2023-10-29 13:58:50 -0500

    Initial commit

---
+++ src/test/java/com/example/postworksesion03joseluis/PostworkSesion03JoseLuisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postworksesion03joseluis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostworkSesion03JoseLuisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
